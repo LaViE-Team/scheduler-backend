037313bc63ec7a54c51cc844d10d055a94caa320
dawn <congminh292k@gmail.com>
2022-12-09 22:33:02 +0700

    reformat csv

--- src/csv/csv.controller.ts
+++ src/csv/csv.controller.ts
@@ -19,16 +19,43 @@ export class CsvController {
         const lines = file.buffer.toString().split('\r\n');
         lines.shift();
 
-        const result = [];
+        const subjects = {};
         for (const line of lines) {
             const lineArr = line.split(',');
+            const subjectCode = lineArr[0];
+            const classId = lineArr[1];
+            const subjectName = lineArr[2];
+            const day = lineArr[3];
+            const startTime = lineArr[4];
+            const endTime = lineArr[5];
+
+            if (subjectCode in subjects) {
+                subjects[subjectCode]['classes'].push({
+                    classId: classId,
+                    day: day,
+                    startTime: startTime,
+                    endTime: endTime,
+                });
+            } else {
+                subjects[subjectCode] = {
+                    subjectName: subjectName,
+                    classes: [
+                        {
+                            classId: classId,
+                            day: day,
+                            startTime: startTime,
+                            endTime: endTime,
+                        },
+                    ],
+                };
+            }
+        }
+
+        const result = [];
+        for (const [key, value] of Object.entries(subjects)) {
             result.push({
-                subjectCode: lineArr[0],
-                classId: lineArr[1],
-                subjectName: lineArr[2],
-                day: lineArr[3],
-                startTime: lineArr[4],
-                endTime: lineArr[5],
+                subjectCode: key,
+                ...(value as object),
             });
         }
         return result;
