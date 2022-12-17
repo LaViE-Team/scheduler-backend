8fc6fb356522564520df140b480ba899d24f9558
dawn <congminh292k@gmail.com>
2022-12-17 09:56:50 +0700

    handle null-input case

--- src/timetable/timetable.controller.ts
+++ src/timetable/timetable.controller.ts
@@ -14,6 +14,9 @@ export class TimetableController {
     async generateTimetable(@Req() req) {
         const user = req.user
         const desiredSubjects = req.body
+        const result = { highDensity: [], lowDensity: [] }
+        if (!desiredSubjects) return result
+
         const { highDensity, lowDensity } = await this.timetableService.generateTimetable(
             user.username,
             desiredSubjects,
@@ -22,7 +25,6 @@ export class TimetableController {
         const classList: any[] = await this.cacheManager.get(`class_list_${user.username}`)
 
         // Get 10 timetable of each type
-        const result = { highDensity: [], lowDensity: [] }
         for (let i = 0; i < 10; i++) {
             if (highDensity.length) {
                 const highDensityTimetable = Array.from(new Set(highDensity.shift()))
