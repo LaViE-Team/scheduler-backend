d95a4c6657d4514216c32e973fb2ed11d4ae6e81
dawn <congminh292k@gmail.com>
2023-01-31 01:04:06 +0700

    share schedule

--- src/schedule/schedule.service.ts
+++ src/schedule/schedule.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from '../prisma/prisma.service'
+import { UsersService } from '../users/users.service'
 
 @Injectable()
 export class ScheduleService {
-    constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService, private userService: UsersService) {}
 
     async saveSchedule(username: string, fileName: string) {
         return this.prisma.schedule.create({
@@ -33,5 +34,52 @@ export class ScheduleService {
                 },
             })
             .then((userSchedules) => userSchedules.map((userSchedule) => ({ ...userSchedule.schedule })))
+    }
+    async shareSchedule(username: string, shareInfo: { shareWith: string; scheduleId: number }) {
+        // Check if user has the schedule
+        const hasSchedule = await this.prisma.user_schedule.findFirst({
+            where: {
+                username: username,
+                schedule_id: shareInfo.scheduleId,
+                shared: false,
+            },
+        })
+        if (!hasSchedule)
+            return {
+                status: 'failed',
+                msg: 'schedule not exist',
+            }
+
+        // Check if shareWith - scheduleId record exists
+        const shareRelationExists = await this.prisma.user_schedule.findUnique({
+            where: {
+                schedule_id_username: {
+                    schedule_id: shareInfo.scheduleId,
+                    username: shareInfo.shareWith,
+                },
+            },
+        })
+        if (shareRelationExists)
+            return {
+                status: 'failed',
+                msg: 'duplicate',
+            }
+
+        // Check if shareWith user exists
+        const userShareWithExists = await this.userService.findUser(shareInfo.shareWith)
+        if (!userShareWithExists)
+            return {
+                status: 'failed',
+                msg: 'user not exist',
+            }
+
+        return this.prisma.user_schedule.create({
+            data: {
+                username: shareInfo.shareWith,
+                schedule_id: shareInfo.scheduleId,
+                shared: true,
+                shared_by_username: username,
+            },
+        })
     }
 }
